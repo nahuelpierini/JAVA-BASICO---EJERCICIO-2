d04f5f6d024007b6b31ebbd555e11a5a034bb6de
NAHUEL <nahuelpierini@gmail.com>
2022-04-04 18:13:57 +0200

    Ejercicio 2

---
+++ src/com/openbootcamp/Main.java
@@ -0,0 +1,47 @@
+package com.openbootcamp;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Numeros Enteros
+
+        byte numero1 = 1;
+        short numero2 = 2;
+        int numero3 = 3;
+        long numero4 = 4;
+
+        System.out.println("Numeros enteros: " + numero1 + " , " + numero2 + " , " + numero3 + " , " + numero4);
+
+
+        //Numeros Decimales
+
+        float decimal1 = 5.5f;
+        double decimal2 = 6.6d;
+
+        System.out.println("Numeros decimales: " + decimal1 + " , " + decimal2);
+
+
+        //Booleanos
+
+        boolean verdadero = true;
+        boolean falso = false;
+
+        System.out.println("Booleanos: " + verdadero + " , " + falso);
+
+        // Caracter
+        char caracter1 = 'a';
+
+        System.out.println("Caracter: " + caracter1);
+
+        // Cadena de Texto
+        String string1 = "string1";
+        System.out.println("Cadena de Texto: " + string1);
+
+
+
+
+
+    }
+
+}
